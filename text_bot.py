cd55b757e6779dadb1b76a016af47fc38b3fcece
threatthreat <grezy8007@gmail.com>
2025-04-03 12:31:14 +0530

    Delete text_bot.py

--- text_bot.py
+++
@@ -1,9 +0,0 @@
-import smtplib, ssl
-from config import *
-
-message = """\ This message is sent from Python."""
-
-context = ssl.create_default_context()
-with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
-    server.login(sender_email, password)
-    server.sendmail(sender_email, receiver_email, message)
